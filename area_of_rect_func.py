ca45b3734133b887b5cfa5b589bb6a0040906641
vaidai01 <159688506+vaidai01@users.noreply.github.com>
2025-01-12 22:43:14 +0530

    Add files via upload

---
+++ area_of_rect_func.py
@@ -0,0 +1,6 @@
+def area_of_rectangle(length, width):
+    return length * width
+length = eval(input("length:"))
+width = eval(input("width:"))
+result = area_of_rectangle(length, width)
+print("Area of rectangle: {result}")
